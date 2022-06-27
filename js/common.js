86ffdf5fec8c0cac9824ae586f8527d248848a85
zhibinniu <1677014995@qq.com>
2022-06-27 09:08:27 +0800

    项目初始化文件准备

---
+++ js/common.js
@@ -0,0 +1,7 @@
+// 上面这个代码处理过度动画（默认加上不用管）
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.body.classList.add('sidenav-pinned')
+    document.body.classList.add('ready')
+  }, 200)
+})
