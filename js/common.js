797e12b7baf321d19e0c00e1092c88a56714310b
zhibinniu <1677014995@qq.com>
2022-06-27 16:08:22 +0800

    使用请求拦截器,获取dashbord数据

--- js/common.js
+++ js/common.js
@@ -5,3 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+axios.defaults.baseURL='http://ajax-api.itheima.net'
+// axios的请求拦截器(一个函数)
+// 作用:对本次请求做一些前置处理
+// 作用:可以统一携带一些参数(例如请求头+token)
+axios.interceptors.request.use(function(config) {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = token
+  }
+  return config
+}, function(error) {
+  return Promise.reject(error)
+})
